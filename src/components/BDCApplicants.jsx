966086ef9911127f9769e7c4c698529fa15279a3
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-07-13 16:07:31 -0300

    Adds base structure of the project

---
+++ src/components/BDCApplicants.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BDCApplicants = () => {
+  return (
+    <div>BDCApplicants</div>
+  )
+}
+
+export default BDCApplicants
---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+// SECTIONS
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Facilities from "./Facilities.jsx"
+import Integration from "./Integration.jsx"
+import Solutions from "./Solutions.jsx";
+import BDCApplicants from "./BDCApplicants.jsx";
+import Form from "./Form.jsx";
+import Footer from "./Footer";
+
+
+
+
+export{
+    // SECTIONS
+    Navbar,
+    Hero,
+    Facilities,
+    Integration,
+    Solutions,
+    BDCApplicants,
+    Form,
+    Footer
+};

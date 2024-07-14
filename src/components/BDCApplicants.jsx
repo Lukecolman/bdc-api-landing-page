3504700bdfdb0a5d843f3c38d04f81bb4f1473d5
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-07-14 19:02:57 -0300

    adds bdcapplicants section

--- src/components/BDCApplicants.jsx
+++ src/components/BDCApplicants.jsx
@@ -2,7 +2,141 @@ import React from 'react'
 
 const BDCApplicants = () => {
   return (
-    <div>BDCApplicants</div>
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+      {/* <div>
+        <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
+          Brand new
+        </p>
+      </div> */}
+      <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+        The quick, brown fox jumps over a lazy dog
+      </h2>
+      <p className="text-base text-gray-700 md:text-lg">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque rem aperiam, eaque ipsa quae.
+      </p>
+    </div>
+    <div className="grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2">
+      <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div className="mr-4">
+          <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              className="w-10 h-10 text-green-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h6 className="mb-3 text-xl font-bold leading-5">The deep ocean</h6>
+          <p className="mb-3 text-sm text-gray-900">
+            A flower in my garden, a mystery in my panties. Heart attack never
+            stopped old Big Bear. I didn't even know we were calling him Big
+            Bear. We never had the chance to.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div className="mr-4">
+          <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              className="w-10 h-10 text-green-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h6 className="mb-3 text-xl font-bold leading-5">
+            When has justice
+          </h6>
+          <p className="mb-3 text-sm text-gray-900">
+            Rough pomfret lemon shark plownose chimaera southern sandfish
+            kokanee northern sea robin Antarctic cod. Yellow-and-black
+            triplefin gulper South American Lungfish mahi-mahi, butterflyfish
+            glass catfish soapfish ling gray mullet!
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div className="mr-4">
+          <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              className="w-10 h-10 text-green-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h6 className="mb-3 text-xl font-bold leading-5">
+            Organically grow
+          </h6>
+          <p className="mb-3 text-sm text-gray-900">
+            A slice of heaven. O for awesome, this chocka full cuzzie is as
+            rip-off as a cracker. Meanwhile, in behind the bicycle shed,
+            Hercules Morse, as big as a horse and Mrs Falani were up to no
+            good with a bunch of crook pikelets.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+        <div className="mr-4">
+          <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+            <svg
+              className="w-10 h-10 text-green-400"
+              stroke="currentColor"
+              viewBox="0 0 52 52"
+            >
+              <polygon
+                strokeWidth="3"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                fill="none"
+                points="29 13 14 29 25 29 23 39 38 23 27 23"
+              />
+            </svg>
+          </div>
+        </div>
+        <div>
+          <h6 className="mb-3 text-xl font-bold leading-5">
+            A slice of heaven
+          </h6>
+          <p className="mb-3 text-sm text-gray-900">
+            Disrupt inspire and think tank, social entrepreneur but
+            preliminary thinking think tank compelling. Inspiring, invest
+            synergy capacity building, white paper; silo, unprecedented
+            challenge B-corp problem-solvers.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 

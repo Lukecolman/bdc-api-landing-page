044771eeffa1112f00ab64f84128bf04033ab04e
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-07-14 16:40:47 -0300

    update facilities style

--- src/components/Facilities.jsx
+++ src/components/Facilities.jsx
@@ -2,32 +2,11 @@ import React from 'react'
 
 const Facilities = () => {
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="px-4 pt-20 pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pb-20 lg:pt-36">
     <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
       <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
         <span className="relative inline-block">
-          <svg
-            viewBox="0 0 52 24"
-            fill="currentColor"
-            className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-          >
-            <defs>
-              <pattern
-                id="d9d7687a-355f-4502-8ec4-7945db034688"
-                x="0"
-                y="0"
-                width=".135"
-                height=".30"
-              >
-                <circle cx="1" cy="1" r=".7" />
-              </pattern>
-            </defs>
-            <rect
-              fill="url(#d9d7687a-355f-4502-8ec4-7945db034688)"
-              width="52"
-              height="24"
-            />
-          </svg>
+
           <span className="relative">The</span>
         </span>{' '}
         quick, brown fox jumps over a lazy dog
@@ -41,7 +20,7 @@ const Facilities = () => {
       <div className="p-5 duration-300 transform bg-white border-l-8 rounded shadow-sm hover:-translate-y-2 hover:shadow-lg">
         <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
           <svg
-            className="w-10 h-10 text-deep-purple-accent-400"
+            className="w-10 h-10 text-green-400"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -63,7 +42,7 @@ const Facilities = () => {
       <div className="p-5 duration-300 transform bg-white border-l-8 rounded shadow-sm hover:-translate-y-2 hover:shadow-lg">
         <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
           <svg
-            className="w-10 h-10 text-deep-purple-accent-400"
+            className="w-10 h-10 text-green-400"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -86,7 +65,7 @@ const Facilities = () => {
       <div className="p-5 duration-300 transform bg-white border-l-8 rounded shadow-sm hover:-translate-y-2 hover:shadow-lg">
         <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
           <svg
-            className="w-10 h-10 text-deep-purple-accent-400"
+            className="w-10 h-10 text-green-400"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -109,7 +88,7 @@ const Facilities = () => {
       <div className="p-5 duration-300 transform bg-white border-l-8 rounded shadow-sm hover:-translate-y-2 hover:shadow-lg">
         <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
           <svg
-            className="w-10 h-10 text-deep-purple-accent-400"
+            className="w-10 h-10 text-green-400"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -132,7 +111,7 @@ const Facilities = () => {
       <div className="p-5 duration-300 transform bg-white border-l-8 rounded shadow-sm hover:-translate-y-2 hover:shadow-lg">
         <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
           <svg
-            className="w-10 h-10 text-deep-purple-accent-400"
+            className="w-10 h-10 text-green-400"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -155,7 +134,7 @@ const Facilities = () => {
       <div className="p-5 duration-300 transform bg-white border-l-8 rounded shadow-sm hover:-translate-y-2 hover:shadow-lg">
         <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
           <svg
-            className="w-10 h-10 text-deep-purple-accent-400"
+            className="w-10 h-10 text-green-400"
             stroke="currentColor"
             viewBox="0 0 52 52"
           >
@@ -179,7 +158,7 @@ const Facilities = () => {
     {/* <div className="text-center">
       <a
         href="/"
-        className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+        className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md md:w-auto bg-green-400 hover:bg-green-700 focus:shadow-outline focus:outline-none"
       >
         Learn more
       </a>

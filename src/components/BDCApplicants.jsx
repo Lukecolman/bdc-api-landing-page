d82fe74a10a9c2c4e93a13c362367bdadd8e7533
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-07-16 00:53:33 -0300

    add constans and minor changes

--- src/components/BDCApplicants.jsx
+++ src/components/BDCApplicants.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { bdcApplicants } from '../constants';
 
 const BDCApplicants = () => {
+  const applicant = bdcApplicants[0];
+
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
     <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-5xl md:mb-12 bg-green-400">
@@ -10,10 +13,10 @@ const BDCApplicants = () => {
         </p>
       </div> */}
       <h2 className="max-w-7xl mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:text-6xl md:mx-auto text-balance bg-red-300">
-      ¿Quiénes ya aplicaron BDC Conecta a su modelo de negocio?
+       {applicant.title}
       </h2>
       <p className="max-w-2xl mx-auto text-base text-gray-700 md:text-lg bg-blue-300 text-pretty">
-      Nuestro universo de BDC Conecta nos permite ir directo a la resolución o mejora de tus procesos específicos, como ya lo hicimos con:
+      {applicant.subtitle}
       </p>
     </div>
     <div className="grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2">
@@ -36,11 +39,10 @@ const BDCApplicants = () => {
           </div>
         </div>
         <div>
-          <h6 className="mb-3 text-xl font-bold leading-5">The deep ocean</h6>
+          <h6 className="mb-3 text-xl font-bold leading-5">            {applicant.item1[0].title}
+          </h6>
           <p className="mb-3 text-sm text-gray-900">
-            A flower in my garden, a mystery in my panties. Heart attack never
-            stopped old Big Bear. I didn't even know we were calling him Big
-            Bear. We never had the chance to.
+          {applicant.item1[0].content}
           </p>
         </div>
       </div>
@@ -64,13 +66,10 @@ const BDCApplicants = () => {
         </div>
         <div>
           <h6 className="mb-3 text-xl font-bold leading-5">
-            When has justice
+          {applicant.item2[0].title}
           </h6>
           <p className="mb-3 text-sm text-gray-900">
-            Rough pomfret lemon shark plownose chimaera southern sandfish
-            kokanee northern sea robin Antarctic cod. Yellow-and-black
-            triplefin gulper South American Lungfish mahi-mahi, butterflyfish
-            glass catfish soapfish ling gray mullet!
+          {applicant.item2[0].content}
           </p>
         </div>
       </div>
@@ -94,13 +93,10 @@ const BDCApplicants = () => {
         </div>
         <div>
           <h6 className="mb-3 text-xl font-bold leading-5">
-            Organically grow
+          {applicant.item3[0].title}
           </h6>
           <p className="mb-3 text-sm text-gray-900">
-            A slice of heaven. O for awesome, this chocka full cuzzie is as
-            rip-off as a cracker. Meanwhile, in behind the bicycle shed,
-            Hercules Morse, as big as a horse and Mrs Falani were up to no
-            good with a bunch of crook pikelets.
+          {applicant.item3[0].content}
           </p>
         </div>
       </div>
@@ -124,13 +120,10 @@ const BDCApplicants = () => {
         </div>
         <div>
           <h6 className="mb-3 text-xl font-bold leading-5">
-            A slice of heaven
+            {applicant.item4[0].title}
           </h6>
           <p className="mb-3 text-sm text-gray-900">
-            Disrupt inspire and think tank, social entrepreneur but
-            preliminary thinking think tank compelling. Inspiring, invest
-            synergy capacity building, white paper; silo, unprecedented
-            challenge B-corp problem-solvers.
+          {applicant.item4[0].content}
           </p>
         </div>
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,116 @@
+// TODO: 
+// - TEXTOS HEADER / FORM / FOOTER
+
+// HERO BANNER
+export const heroBanner = {
+    title: "Eficiencia en cobros y pagos de tu negocio con BDC Conecta",
+    // contentTEST: "Transforma tu negocio con nuestras soluciones integradas. Mejora la eficiencia, seguridad y experiencia del cliente con BDC Conecta.", 
+    // contentTEST2: "Transforma tu negocio con BDC Conecta. Integra tu sistema de gestión y optimiza tus operaciones financieras con nuestras soluciones tecnológicas de APIs. Acelera tus procesos, reduce costos y maximiza la eficiencia.", 
+    content: "Integrá tu sistema de gestión, acelerá tus cobros y pagos, y desbloqueá un nuevo nivel de eficiencia. Dale un giro disruptivo a tu negocio con nuestras soluciones tecnológicas de APIs BDC Conecta. Con nuestro sistema de APIs a medida, optimizás tiempos, reducís costos y maximizás recursos, llevando tu empresa a un nuevo modelo de administración y agilidad en tesorería.", 
+    button: {
+      text: "Descubre más",
+      link: "/descubrir-mas"
+    },
+    imageUrl: '' 
+  };
+
+// FACILITIES
+export const facilities = {
+    sectionTitle: "Facilidades para tu negocio",
+    subtitle: "¿En qué facilita el desarrollo de mi negocio tener BDC Conecta?",
+    items: [
+      {
+        id: 1,
+        icon: '',
+        content: "Obtené información en tiempo real y detallada de todas tus operaciones."
+      },
+      {
+        id: 2,
+        icon: '',
+        content: "Acelerá el proceso de recaudación realizando transacciones instantáneamente."
+      },
+      {
+        id: 3,
+        icon: '',
+        content: "Identificá cada operación por cliente, producto o servicio, facilitando tu conciliación bancaria."
+      },
+      {
+        id: 4,
+        icon: '',
+        content: "Garantizá transacciones seguras tanto para tu empresa como para tus clientes."
+      },
+      {
+        id: 5,
+        icon: '',
+        content: "Soluciones que permiten un control total y la captura precisa de datos."
+      },
+      {
+        id: 6,
+        icon: '',
+        content: "Cumplimiento normativo y seguridad garantizados en todas las operaciones."
+      }
+    ]
+  };
 
 
+// INTEGRATION
+export const integration = {
+    sectionTitle: "¿Cómo es el proceso para integrar BDC Conecta?",
+    items: [
+      {
+        id: 1,
+        icon: '',
+        content: "Entendemos tu marca, comprendiendo las particularidades de tu modelo de negocio."
+      },
+      {
+        id: 2,
+        icon: '',
+        content: "Un asesor especializado crea una propuesta específica de todo nuestro universo de APIs."
+      },
+      {
+        id: 3,
+        icon: '',
+        content: "Te ayudamos a integrar las APIs a tus procesos en conjunto con tu equipo."
+      },
+      {
+        id: 4,
+        icon: '',
+        content: "Agilizás tus procesos de forma segura, brindando mejores experiencias a tus clientes."
+      }
+    ]
+  };
+  
+
+// SOLUTIONS
+export const solutions = [
+    {
+      sectionTitle: "Soluciones de BDC Conecta",
+      items: [
+        {
+          id: 1,
+          image: '',
+          title: "Cobranzas y Pagos",
+          content: "Automatizá los pagos y cobros de tus clientes y proveedores, ahorrando tiempo y costos de gestión. Con BDC Conecta podés consultar y realizar transferencias a CBU, CVU y Alias, y gestionar proveedores de forma eficiente. Transforma la gestión de tus transacciones y llevá tu negocio al próximo nivel con nuestras soluciones tecnológicas."
+        },
+        {
+          id: 2,
+          image: '',
+          title: "Billetera Virtual",
+          content: "Nuestra solución ofrece diversas funcionalidades para optimizar la gestión de las transacciones en tu billetera: altas y cambio de alias de CVU, consulta, envío y recepción de transferencias entre CVU/CBU, y notificaciones de crédito en CVU, y posibilidad de financiación a tus clientes."
+        },
+        {
+          id: 3,
+          image: '',
+          title: "Información de la cuenta",
+          content: "Te permite gestionar integralmente las cuentas de tus clientes en BDC Conecta. Accedé a los movimientos de cuenta, consultá datos mediante alias, CBU o CVU, y validá la información asociada a cada uno. Identificá el origen de transferencias recibidas y obtené el saldo y el detalle de cada cuenta de manera sencilla y eficiente."
+        }
+      ]
+    }
+  ];
+  
+
+
+// BDC APPLICANTS
 export const bdcApplicants = [
     {
       title: "¿Quiénes ya aplicaron BDC Conecta a su modelo de negocio?",
@@ -37,6 +148,5 @@ export const bdcApplicants = [
         },
       ],
     },
-
   ];
-
+  

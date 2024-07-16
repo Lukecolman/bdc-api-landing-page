6c4fe4d53bdb6a819b2a693fb1f6d83280f39cc0
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-07-16 19:59:24 -0300

    updates form design

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,90 +1,86 @@
 import React from 'react';
+import { form } from '../constants';
 
 const Form = () => {
     return (
-        <div className='overflow-hidden bg-gray-900'>
-            <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
-                <div className='flex flex-col items-center justify-between xl:flex-row'>
-                    <div className='w-full max-w-xl mb-12 xl:pr-16 xl:mb-0 xl:w-7/12'>
-                        <h2 className='max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none'>
-                            The quick, brown fox <br className='hidden md:block' />
-                            jumps over a <span className='text-teal-accent-400'>lazy dog</span>
+        <div className='overflow-hidden bg-gray-900 md:px-8'>
+            <div className='px-3.5 py-16 mx-auto  md:max-w-full  md:px-24 lg:px-8 lg:py-20 bg-green-400'>
+                <div className='flex flex-col items-center justify-between'>
+                    <div className='w-full text-center bg-red-400'>
+                        <h2 className='max-w-lg mx-auto mb-6 font-sans text-3xl font-semibold text-white sm:text-5xl sm:leading-none bg-blue-500'>
+                            {form.title}
                         </h2>
-                        <p className='max-w-xl mb-4 text-base text-gray-400 md:text-lg'>
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-                            laudan, totam rem aperiam, eaque ipsa quae.
-                        </p>
-                        <a
-                            href='/'
-                            aria-label=''
-                            className='inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700'>
-                            Learn more
-                            <svg className='inline-block w-3 ml-2' fill='currentColor' viewBox='0 0 12 12'>
-                                <path d='M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z' />
-                            </svg>
-                        </a>
-                    </div>
-                    <div className='w-full max-w-xl xl:px-8 xl:w-5/12'>
-                        <div className='relative'>
-                            <svg
-                                viewBox='0 0 52 24'
-                                fill='currentColor'
-                                className='absolute bottom-0 right-0 z-0 hidden w-32 -mb-8 -mr-20 text-teal-accent-400 lg:w-32 lg:-mr-16 sm:block'>
-                                <defs>
-                                    <pattern
-                                        id='766323e1-e594-4ffd-a688-e7275079d540'
-                                        x='0'
-                                        y='0'
-                                        width='.135'
-                                        height='.30'>
-                                        <circle cx='1' cy='1' r='.7' />
-                                    </pattern>
-                                </defs>
-                                <rect fill='url(#766323e1-e594-4ffd-a688-e7275079d540)' width='52' height='24' />
-                            </svg>
-                            <div className='relative bg-white rounded shadow-2xl p-7 sm:p-10'>
-                                <h3 className='mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl'>
-                                    Sign up for updates
-                                </h3>
-                                <form>
-                                    <div className='mb-1 sm:mb-2'>
-                                        <label htmlFor='name' className='inline-block mb-1 font-medium'>
-                                            Name
-                                        </label>
+                        {/* <p className='max-w-xl mb-4 text-base text-gray-400 md:text-lg'>
+                            sub text
+                        </p> */}
+                        <div className='relative  bg-violet-600 max-w-md mx-auto'>
+                            <form>
+                                <div className='mb-1 sm:mb-2 text-xs'>
+                                    <div className='grid grid-cols-2 gap-x-5 gap-y-3'>
                                         <input
-                                            placeholder='John Doe'
+                                            placeholder='Nombre y Apellido'
                                             required
                                             type='text'
-                                            className='flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline'
+                                            className='flex-grow w-full h-9 px-3.5  transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-green-400 focus:outline-none focus:shadow-outline'
                                             id='name'
                                             name='name'
                                         />
-                                    </div>
-                                    <div className='mb-1 sm:mb-2'>
-                                        <label htmlFor='email' className='inline-block mb-1 font-medium'>
-                                            E-mail
-                                        </label>
+
                                         <input
-                                            placeholder='john.doe@example.org'
+                                            placeholder='Email'
                                             required
-                                            type='text'
-                                            className='flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline'
+                                            type='email'
+                                            className='flex-grow w-full h-9 px-3.5  transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-green-400 focus:outline-none focus:shadow-outline'
                                             id='email'
                                             name='email'
                                         />
+
+                                        <input
+                                            placeholder='Teléfono'
+                                            required
+                                            type='tel'
+                                            className='flex-grow w-full h-9 px-3.5  transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-green-400 focus:outline-none focus:shadow-outline'
+                                            id='phone'
+                                            name='phone'
+                                        />
+
+                                        <input
+                                            placeholder='CUIT'
+                                            required
+                                            type='text'
+                                            className='flex-grow w-full h-9 px-3.5  transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-green-400 focus:outline-none focus:shadow-outline'
+                                            id='cuit'
+                                            name='cuit'
+                                        />
                                     </div>
-                                    <div className='mt-4 mb-2 sm:mb-4'>
-                                        <button
-                                            type='submit'
-                                            className='inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none'>
-                                            Subscribe
-                                        </button>
-                                    </div>
-                                    <p className='text-xs text-gray-600 sm:text-sm'>
-                                        We respect your privacy. Unsubscribe at any time.
-                                    </p>
-                                </form>
-                            </div>
+                                    <input
+                                        placeholder='Razón Social'
+                                        required
+                                        type='text'
+                                        className='flex-grow w-full h-9 px-3.5  transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-green-400 focus:outline-none focus:shadow-outline mt-3'
+                                        id='razonSocial'
+                                        name='razonSocial'
+                                    />
+
+                                    <textarea
+                                        placeholder='Solución de tu interés'
+                                        required
+                                        className='flex-grow w-full h-9 p-3.5  transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-green-400 focus:outline-none focus:shadow-outline mt-3 min-h-20'
+                                        id='message'
+                                        name='message'
+                                    />
+                                </div>
+                                <div className='mt-6 mb-2 sm:mb-4'>
+                                    <button
+                                        type='submit'
+                                        className='inline-flex items-center justify-center w-full h-9 px-6 font-bold tracking-wide text-black transition duration-200 rounded shadow-md bg-green-400 hover:bg-green-700 focus:shadow-outline focus:outline-none'>
+                                        {form.buttonText}
+                                    </button>
+                                </div>
+                                {/* <p className='text-xs text-gray-600 sm:text-sm'>
+                                    policies
+                                </p> */}
+                            </form>
                         </div>
                     </div>
                 </div>

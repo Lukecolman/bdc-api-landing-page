d6e48564f1f19b2162a63451cc2a70bdd81720ee
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-07-16 00:18:36 -0300

    adds bdc applicants and form base

--- src/components/BDCApplicants.jsx
+++ src/components/BDCApplicants.jsx
@@ -3,18 +3,17 @@ import React from 'react'
 const BDCApplicants = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-    <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-5xl md:mb-12 bg-green-400">
       {/* <div>
         <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
           Brand new
         </p>
       </div> */}
-      <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-        The quick, brown fox jumps over a lazy dog
+      <h2 className="max-w-7xl mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:text-6xl md:mx-auto text-balance bg-red-300">
+      ¿Quiénes ya aplicaron BDC Conecta a su modelo de negocio?
       </h2>
-      <p className="text-base text-gray-700 md:text-lg">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque rem aperiam, eaque ipsa quae.
+      <p className="max-w-2xl mx-auto text-base text-gray-700 md:text-lg bg-blue-300 text-pretty">
+      Nuestro universo de BDC Conecta nos permite ir directo a la resolución o mejora de tus procesos específicos, como ya lo hicimos con:
       </p>
     </div>
     <div className="grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2">
---
+++ src/constants/index.js
@@ -0,0 +1,42 @@
+
+
+export const bdcApplicants = [
+    {
+      title: "¿Quiénes ya aplicaron BDC Conecta a su modelo de negocio?",
+      subtitle: "Nuestro universo de BDC Conecta nos permite ir directo a la resolución o mejora de tus procesos específicos, como ya lo hicimos con:",
+      item1: [
+        {
+          id: 1,
+          icon: '',
+          title: "PSP",
+          content: "Nuestro universo de BDC Conecta nos permite ir directo a la resolución o mejora de tus procesos específicos, como ya lo hicimos con:",
+        },
+      ],
+      item2: [
+        {
+          id: 2,
+          icon: '',
+          title: "Colegios, gimnasios y clubes",
+          content: "Realizan cobros de tarjeta crédito y débito, pago masivo a proveedores automatizando procesos y reduciendo recursos, garantizando la optimización de tesorería.",
+        },
+      ],
+      item3: [
+        {
+          id: 3,
+          icon: '',
+          title: "Salud",
+          content: "Nuestro cliente puede realizar pagos masivos y programados a toda su nómina y proveedores, facilitando su gestión interna y externa. Recauda de manera ágil sus cuotas mensuales e identifica de manera precisa toda la información de las cuentas de sus asociados y proveedores.",
+        },
+      ],
+      item4: [
+        {
+          id: 4,
+          icon: '',
+          title: "Consorcios",
+          content: "El cliente desarrolló su propia billetera virtual, desde la cual gestiona cobros y pagos masivos, creación de cuentas CVU a cuenta de terceros. También con esta solución financia a sus clientes. Todo esto le agiliza sus operaciones, ahorra costos operativos y tiempo y le permite cumplir con todas las normativas del BCRA.",
+        },
+      ],
+    },
+
+  ];
+

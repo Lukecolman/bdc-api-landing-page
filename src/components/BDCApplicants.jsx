46e7d4ffc3c348d7b4931c763a4933f22e42ac8d
Luke <lucas.a.colman@gmail.com>
2024-07-28 21:36:34 -0300

    change icons

--- src/components/BDCApplicants.jsx
+++ src/components/BDCApplicants.jsx
@@ -1,104 +1,132 @@
-import React from 'react';
-import { bdcApplicants } from '../constants';
-import { AiOutlineDollarCircle } from 'react-icons/ai';
-import { AiOutlineCreditCard } from 'react-icons/ai';
+import React from "react";
+import { bdcApplicants } from "../constants";
+import {
+  AiOutlineWallet,
+  AiOutlineShoppingCart,
+  AiOutlineMedicineBox,
+  AiOutlineRead,
+} from "react-icons/ai";
+import { CgGym } from "react-icons/cg";
+import { HiOutlineBuildingOffice } from "react-icons/hi2";
 
-import { AiOutlineCluster } from 'react-icons/ai';
-import { AiOutlinePartition } from 'react-icons/ai';
-
-
-/* TODO: BUSCAR NUEVOS ICONOS Y REVISAR LOS EXISTENTES */
 const BDCApplicants = () => {
-    const applicant = bdcApplicants[0];
+  const applicant = bdcApplicants[0];
 
-    return (
-        <div className='font-inter px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
-            <div className='max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-5xl md:mb-16 '>
-                {/* <div>
+  return (
+    <div className="font-inter px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-5xl md:mb-16 ">
+        {/* <div>
         <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
           Brand new
         </p>
       </div> */}
-                <h2 className='max-w-7xl mb-6 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:text-6xl md:mx-auto text-balance' dangerouslySetInnerHTML={{ __html: applicant.title }}/>
-                <p className='max-w-2xl mx-auto text-base text-gray-700 md:text-lg  text-pretty'>
-                    {applicant.subtitle}
-                </p>
+        <h2
+          className="max-w-7xl mb-6 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:text-6xl md:mx-auto text-balance"
+          dangerouslySetInnerHTML={{ __html: applicant.title }}
+        />
+        <p className="max-w-2xl mx-auto text-base text-gray-700 md:text-lg  text-pretty">
+          {applicant.subtitle}
+        </p>
+      </div>
+      <div className="grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2">
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md">
+              <AiOutlineWallet className="w-7 h-7 text-green-400" />
             </div>
-            <div className='grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2'>
-                <div className='flex flex-col max-w-md sm:mx-auto sm:flex-row'>
-                    <div className='mr-4'>
-                        <div className='flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md'>
-                            <AiOutlineDollarCircle className='w-7 h-7 text-green-400' />
-                        </div>
-                    </div>
-                    <div>
-                        <h6 className='mb-3 text-xl font-bold leading-5'> {applicant.item1[0].title}</h6>
-                        <p className='mb-3 text-sm text-gray-900'>{applicant.item1[0].content}</p>
-                    </div>
-                </div>
-                <div className='flex flex-col max-w-md sm:mx-auto sm:flex-row'>
-                    <div className='mr-4'>
-                        <div className='flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md'>
-                            <AiOutlineCreditCard className='w-7 h-7 text-green-400' />
-                        </div>
-                    </div>
-                    <div>
-                        <h6 className='mb-3 text-xl font-bold leading-5'>{applicant.item2[0].title}</h6>
-                        <p className='mb-3 text-sm text-gray-900'>{applicant.item2[0].content}</p>
-                    </div>
-                </div>
-
-                <div className='flex flex-col max-w-md sm:mx-auto sm:flex-row'>
-                    <div className='mr-4'>
-                        <div className='flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md'>
-                            <AiOutlineCluster className='w-7 h-7 text-green-400' />
-                        </div>
-                    </div>
-                    <div>
-                        <h6 className='mb-3 text-xl font-bold leading-5'>{applicant.item3[0].title}</h6>
-                        <p className='mb-3 text-sm text-gray-900'>{applicant.item3[0].content}</p>
-                    </div>
-                </div>
-
-                <div className='flex flex-col max-w-md sm:mx-auto sm:flex-row'>
-                    <div className='mr-4'>
-                        <div className='flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md'>
-                            <AiOutlinePartition className='w-7 h-7 text-green-400' />
-                        </div>
-                    </div>
-                    <div>
-                        <h6 className='mb-3 text-xl font-bold leading-5'>{applicant.item4[0].title}</h6>
-                        <p className='mb-3 text-sm text-gray-900'>{applicant.item4[0].content}</p>
-                    </div>
-                </div>
-
-                <div className='flex flex-col max-w-md sm:mx-auto sm:flex-row'>
-                    <div className='mr-4'>
-                        <div className='flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md'>
-                            <AiOutlinePartition className='w-7 h-7 text-green-400' />
-                        </div>
-                    </div>
-                    <div>
-                        <h6 className='mb-3 text-xl font-bold leading-5'>{applicant.item5[0].title}</h6>
-                        <p className='mb-3 text-sm text-gray-900'>{applicant.item5[0].content}</p>
-                    </div>
-                </div>
-
-                <div className='flex flex-col max-w-md sm:mx-auto sm:flex-row'>
-                    <div className='mr-4'>
-                        <div className='flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md'>
-                            <AiOutlinePartition className='w-7 h-7 text-green-400' />
-                        </div>
-                    </div>
-                    <div>
-                        <h6 className='mb-3 text-xl font-bold leading-5'>{applicant.item6[0].title}</h6>
-                        <p className='mb-3 text-sm text-gray-900'>{applicant.item6[0].content}</p>
-                    </div>
-                </div>
-
-            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              {" "}
+              {applicant.item1[0].title}
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              {applicant.item1[0].content}
+            </p>
+          </div>
         </div>
-    );
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md">
+              <AiOutlineShoppingCart className="w-7 h-7 text-green-400" />
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              {applicant.item2[0].title}
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              {applicant.item2[0].content}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md">
+              <AiOutlineMedicineBox className="w-7 h-7 text-green-400" />
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              {applicant.item3[0].title}
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              {applicant.item3[0].content}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md">
+              <AiOutlineRead className="w-7 h-7 text-green-400" />
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              {applicant.item4[0].title}
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              {applicant.item4[0].content}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md">
+              <HiOutlineBuildingOffice className="w-7 h-7 text-green-400" />
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              {applicant.item5[0].title}
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              {applicant.item5[0].content}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full shadow-md">
+              <CgGym className="w-7 h-7 text-green-400" />
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              {applicant.item6[0].title}
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              {applicant.item6[0].content}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BDCApplicants;
--- src/components/Opportunities.jsx
+++ src/components/Opportunities.jsx
@@ -3,9 +3,9 @@ import { opportunities } from '../constants';
 import {
     AiOutlineBarChart,
     AiOutlineThunderbolt,
-    AiOutlineSearch,
-    AiOutlineLock,
+    AiOutlineFileProtect,
     AiOutlineControl,
+    AiOutlineDatabase,
     AiOutlineSafety,
 } from 'react-icons/ai';
 
@@ -36,21 +36,21 @@ const Opportunities = () => {
                 </div>
                 <div className='p-5 duration-200 transform bg-white border-l-4 border-bdc-green text-bdc-gray-700 rounded shadow-md hover:-translate-y-2 hover:shadow-lg flex flex-col'>
                     <div className='w-fit p-3 mb-4 rounded-full shadow-md'>
-                        <AiOutlineSearch className='w-7 h-7 text-bdc-green-300' />
+                        <AiOutlineFileProtect className='w-7 h-7 text-bdc-green-300' />
                     </div>
                     <span className='font-bold text-xl mb-2'>{opportunities.items[2].title}</span>
                     <p className='mb-2 font-regular leading-snug text-base text-bdc-gray-600'>{opportunities.items[2].content}</p>
                 </div>
                 <div className='p-5 duration-200 transform bg-white border-l-4 border-bdc-green text-bdc-gray-700 rounded shadow-md hover:-translate-y-2 hover:shadow-lg flex flex-col'>
                     <div className='w-fit p-3 mb-4 rounded-full shadow-md'>
-                        <AiOutlineLock className='w-7 h-7 text-bdc-green-300' />
+                        <AiOutlineControl className='w-7 h-7 text-bdc-green-300' />
                     </div>
                     <span className='font-bold text-xl mb-2'>{opportunities.items[3].title}</span>
                     <p className='mb-2 font-regular leading-snug text-base text-bdc-gray-600'>{opportunities.items[3].content}</p>
                 </div>
                 <div className='p-5 duration-200 transform bg-white border-l-4 border-bdc-green text-bdc-gray-700 rounded shadow-md hover:-translate-y-2 hover:shadow-lg flex flex-col'>
                     <div className='w-fit p-3 mb-4 rounded-full shadow-md'>
-                        <AiOutlineControl className='w-7 h-7 text-bdc-green-300' />
+                        <AiOutlineDatabase className='w-7 h-7 text-bdc-green-300' />
                     </div>
                     <span className='font-bold text-xl mb-2'>{opportunities.items[4].title}</span>
                     <p className='mb-2 font-regular leading-snug text-base text-bdc-gray-600'>{opportunities.items[4].content}</p>

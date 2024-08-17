7f05797acb3cbb958d5ec17fb9bef24f94bf868a
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-08-17 16:25:04 -0300

    setting up slider configuration

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -39,15 +39,51 @@ import 'keen-slider/keen-slider.min.css';
 export default () => {
     const [currentSlide, setCurrentSlide] = useState(0);
     const [loaded, setLoaded] = useState(false);
-    const [sliderRef, instanceRef] = useKeenSlider({
-        initial: 0,
-        slideChanged(slider) {
-            setCurrentSlide(slider.track.details.rel);
+    const [sliderRef, instanceRef] = useKeenSlider(
+        {
+            initial: 0,
+            slideChanged(slider) {
+                setCurrentSlide(slider.track.details.rel);
+            },
+            created() {
+                setLoaded(true);
+            },
         },
-        created() {
-            setLoaded(true);
-        },
-    });
+        [
+            (slider) => {
+                let timeout;
+                let mouseOver = false;
+
+                function clearNextTimeout() {
+                    clearTimeout(timeout);
+                }
+
+                function nextTimeout() {
+                    clearTimeout(timeout);
+                    if (mouseOver) return;
+                    timeout = setTimeout(() => {
+                        slider.next();
+                    }, 2000);
+                }
+
+                slider.on('created', () => {
+                    slider.container.addEventListener('mouseover', () => {
+                        mouseOver = true;
+                        clearNextTimeout();
+                    });
+                    slider.container.addEventListener('mouseout', () => {
+                        mouseOver = false;
+                        nextTimeout();
+                    });
+                    nextTimeout();
+                });
+
+                slider.on('dragStarted', clearNextTimeout);
+                slider.on('animationEnded', nextTimeout);
+                slider.on('updated', nextTimeout);
+            },
+        ]
+    );
 
     return (
         <>
